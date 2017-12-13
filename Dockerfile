7b63e42ab7bfa0b19373e10a70b7704d49ee5767
Nikita <kretov995@gmail.com>
2017-12-13 22:50:55 +0300

    code refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM continuumio/miniconda3
 COPY ./to_grey.py /to_grey.py
+
 ENV F_SUFF="_gr"
+ENV WDIR=""
+ENV BUCKET=""
+
 RUN conda install -y scikit-image   
 VOLUME ["/images"]
 ENTRYPOINT ["python", "/to_grey.py"]
--- to_grey.py
+++ to_grey.py
@@ -2,14 +2,32 @@ from skimage import data, io, filters, exposure
 from skimage.util import img_as_ubyte
 from skimage.morphology import disk
 from skimage.color import rgb2gray
+import boto3
 import os
 
-path = './images/'
-for fl in os.listdir(path):
-    picture = None
-    picture = io.imread(path + fl, True)
+def main_loop(read_f, action_f, save_f, path):
+    for file_name in os.listdir(path):
+        img = read_f(file_name)
+        res_img = action_f(img)
+        save_f(res_img)
+    
+def read_img_from_fs(dir_path, file_name):
+    picture = io.imread(dir_path + file_name, True)
+    return picture
+
+def perform_picture_modification(picture):
     img = img_as_ubyte(picture)
     adj = exposure.adjust_log(img)
     eq = exposure.equalize_hist(adj)
     res = filters.rank.equalize(eq, selem=disk(30))
-    io.imsave(path+'res/'+fl.split('.')[0]+'_{0}.jpg'.format(os.environ['F_SUFF']), res, quality=10)
+    return res
+
+def upload_to_s3(bucket_name, file_name):
+    s3 = boto3.resource('s3')
+    s3.Bucket(bucket_name).download_file(file_name, file_name)
+
+read_f = lambda file_name: read_img_from_fs(os.environ['WDIR'], file_name)
+save_f = lambda file_name: upload_to_s3(os.environ['BUCKET'], file_name)
+action_f = lambda picture: perform_picture_modification(picture)
+
+main_loop(read_f, action_f, save_f, os.environ['WDIR'])
