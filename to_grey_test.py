b454afe97e032871b2ffffde16e6fd0114d11b53
Nikita <kretov995@gmail.com>
2017-12-14 00:05:43 +0300

    formatting

--- to_grey_test.py
+++ to_grey_test.py
@@ -10,7 +10,5 @@ class TestToGrey(unittest.TestCase):
     def test_read_img_from_fs(self):
         self.assertTrue(type(to_grey.read_img_from_fs("./tests/data", "img.jpg")) is numpy.ndarray)
 
-
-
 if __name__ == '__main__':
     unittest.main()
